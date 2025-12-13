fef17ace8bfb64cd6314151e3f73cc6c92944b26
Sagume_Sweet <kaguyakochiya@gmail.com>
2025-12-14 00:54:01 +0800

    fix: 修复引用错误

--- FileScanner/__init__.py
+++ FileScanner/__init__.py
@@ -1,6 +1,6 @@
-from FolderScanner import FolderScanner
-from Processer import IProcesser, BaseProcessResult
-from Logger import IScannerLogger
+from .FolderScanner import FolderScanner
+from .Processer import IProcesser, BaseProcessResult
+from .Logger import IScannerLogger
 
 __all__ = [
     "FolderScanner",
